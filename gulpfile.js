bf3d1fadbe58e8e9eb999e8137d9666077f4bd88
telanks <769967440@qq.com>
2018-01-25 22:32:35 +0800

    gulp编译前先删除文件；嵌套语法

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
-gulp.task('default', () => {
+const del = require('del');
+gulp.task('clean', () => {
+    del('dist');
+})
+gulp.task('default',['clean'], () => {
     return gulp.src('./style/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist'))
