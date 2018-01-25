d90e1c041941990a923d1b74fcbbef16989d4abb
telanks <769967440@qq.com>
2018-01-25 23:12:33 +0800

    css压缩；sass函数

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const del = require('del');
+const cleanCSS = require('gulp-clean-css');
 gulp.task('clean', () => {
     del('dist');
 })
+
 gulp.task('default',['clean'], () => {
     return gulp.src('./style/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass())
+        // .pipe(cleanCSS())
         .pipe(gulp.dest('dist'))
 })
 
