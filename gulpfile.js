ae9409bf00fdc4db4ae4d960050f36ed8794ade9
unknown <tang_xiaodong@Hoperun.com>
2018-01-26 10:45:41 +0800

    增加watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const del = require('del');
 const cleanCSS = require('gulp-clean-css');
+let timer = null
 gulp.task('clean', () => {
-    del('dist');
+   return del('dist');
 })
 
-gulp.task('default',['clean'], () => {
+gulp.task('default', ['clean'], () => {
     return gulp.src('./style/*.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         // .pipe(cleanCSS())
         .pipe(gulp.dest('dist'))
 })
 
+gulp.task('watcher', () => {
+    gulp.watch('./style/**/*', () => {
+        clearTimeout(timer)
+        timer = setTimeout(function () {
+           gulp.run('default')
+        },2000)
+    })
+})
